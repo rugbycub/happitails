1e7ac38283d73d25a666a8bff25b93b0a4d345aa
Charlie Nuttall <CENuttall@mac.com>
2014-01-12 21:58:56 -0800

    added methods

--- shelter.rb
+++ shelter.rb
@@ -8,14 +8,14 @@ class Shelter
 
 	def client_list
 		result = @clients.inject("") do |client_list, client|
-			client_list << "Name: #{client.name}\n"
+			client_list << "Name: #{client.name}          Number of Pets: #{client.num_of_pets}\n"
 		end
 		result
 	end
 
 	def animal_list
 		result = @animals.inject("") do |animal_list, animal|
-			animal_list << "Name: #{animal.name} Species: #{animal.species} Age: #{animal.age}\n"
+			animal_list << "Name: #{animal.name}          Species: #{animal.species} Age: #{animal.age}\n"
 		end
 		result
 	end
